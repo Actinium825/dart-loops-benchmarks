15e0b1b09a00c2a3c5bb1212b12a8d88b6ccbabc
Actinium825 <cruz.d.ace@proton.me>
2025-10-01 13:05:52 +0800

    workflow

--- model/character.dart
+++ model/character.dart
@@ -1,13 +1,6 @@
-enum Gender {
-  male,
-  female,
-}
+enum Gender { male, female }
 
-enum Status {
-  alive,
-  mia,
-  kia,
-}
+enum Status { alive, mia, kia }
 
 class Character {
   final String name;
@@ -45,23 +38,16 @@ class Character {
       death: DateTime.tryParse(json['death'] as String? ?? ''),
       name: json['name'] as String? ?? '',
       nationality: json['nationality'] as String? ?? '',
-      affiliations: (json['affiliations'] as List<dynamic>)
-          .map((e) => e.toString())
-          .toList(),
+      affiliations: (json['affiliations'] as List<dynamic>).map((e) => e.toString()).toList(),
       rank: json['name'] as String? ?? '',
       gender: Gender.values[json['gender'] as int? ?? 0],
       status: Status.values[json['status'] as int? ?? 0],
       height: json['height'] as double? ?? 0,
       hair: json['hair'] as String? ?? '',
       eyes: json['eyes'] as String? ?? '',
-      relatives: (json['relatives'] as List<dynamic>)
-          .map((e) => e.toString())
-          .toList(),
-      weapons:
-          (json['weapons'] as List<dynamic>).map((e) => e.toString()).toList(),
-      equipment: (json['equipment'] as List<dynamic>)
-          .map((e) => e.toString())
-          .toList(),
+      relatives: (json['relatives'] as List<dynamic>).map((e) => e.toString()).toList(),
+      weapons: (json['weapons'] as List<dynamic>).map((e) => e.toString()).toList(),
+      equipment: (json['equipment'] as List<dynamic>).map((e) => e.toString()).toList(),
     );
   }
 }
